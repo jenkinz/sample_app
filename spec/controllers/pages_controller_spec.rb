0a157d3d1340b06026b3067a2b470ba79aae99f6
Brian Jenkins <bj@jenkinz.com>
2012-05-01 15:45:06 -0700

    Added a site layout and home, contact and about views

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,22 @@ require 'spec_helper'
 
 describe PagesController do
 
+  render_views
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/ # response body should not have an empty <body></body> section
     end
   end
 
@@ -13,6 +25,23 @@ describe PagesController do
     it "returns http success" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
